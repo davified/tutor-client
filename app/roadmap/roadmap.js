333c74174d1e31de2b0b05f418f847ec199606c2
David Tan <davified@gmail.com>
2016-08-02 15:37:07 +0800

    first commit

---
+++ app/roadmap/roadmap.js
@@ -0,0 +1,17 @@
+'use strict'
+
+angular.module('myApp.roadmap', ['ngRoute'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/roadmap', {
+      templateUrl: 'roadmap/roadmap.html',
+      controller: 'roadmapCtrl'
+    })
+  }])
+
+  .controller('roadmapCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $http.get('https://learning-ninja-api.herokuapp.com/topics/6').then(function (response) {
+      $scope.topics = response.data
+      console.log($scope.topics)
+    })
+  }])
---
+++ app/roadmap/roadmap_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.roadmap module', function() {
+
+  beforeEach(module('myApp.roadmap'));
+
+  describe('roadmap controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var roadmapCtrl = $controller('roadmapCtrl');
+      expect(roadmapCtrl).toBeDefined();
+    }));
+
+  });
+});

05c0147ca6ef2483a703e9472bb8c4132586956d
David Tan <davified@gmail.com>
2016-08-05 17:33:21 +0800

    added user login form. need to link up with backend models

--- app/components/material-form/material-form.component.js
+++ app/components/material-form/material-form.component.js
@@ -1,32 +1,34 @@
 'use strict'
 
 angular.module('materialForm', ['ngMaterial', 'ngMessages'])
-  .config(function ($mdThemingProvider) {
-    // Configure a dark theme with primary foreground yellow
-    $mdThemingProvider.theme('docs-dark', 'default')
-      .primaryPalette('yellow')
-      .dark()
-  })
   .component('materialForm', {
     templateUrl: 'components/material-form/material-form.template.html',
     controller: [ function MaterialFormCtrl () {
       var self = this
       self.user = {
-        title: 'Developer',
-        email: 'ipsum@lorem.com',
         firstName: '',
         lastName: '',
-        company: 'Google',
-        address: '1600 Amphitheatre Pkwy',
-        city: 'Mountain View',
-        state: 'CA',
-        biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-        postalCode: '94043'
-      }
-      self.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-      'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-      'WY').split(' ').map(function (state) {
-        return {abbrev: state}
-      })
+        ninjaName: '',
+        school: '',
+        password: '',
+        address: '',
+        postalCode: '',
+        birthday: '', // date
+        level: '',
+        aboutMe: ''
+      },
+      self.levels = [
+        'Primary 1',
+        'Primary 2',
+        'Primary 3',
+        'Primary 4',
+        'Primary 5',
+        'Primary 6',
+        'Secondary 1',
+        'Secondary 2',
+        'Secondary 3',
+        'Secondary 4',
+        'Secondary 5'
+      ]
     }]
   })

63bb7442639f3c2a2e6521308948d043d0abd53d
David Tan <davified@gmail.com>
2016-08-07 10:38:01 +0800

    added login html views and teal theme

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,7 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.roadmap',
   'myApp.exercise',
+  'myApp.login',
   'myApp.version',
   'ngMaterial',
   'myApp.material',
---
+++ app/login/login.js
@@ -0,0 +1,12 @@
+angular.module('myApp.login', ['ngRoute'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {
+      templateUrl: 'login/login.html',
+      controller: 'loginCtrl'
+    })
+  }])
+
+  .controller('loginCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+
+  }])

cd8592d319174516590810b91c672c7b352f345b
David Tan <davified@gmail.com>
2016-08-08 15:16:33 +0800

    able to shuffle through questions and verify if answer is correct

--- app/roadmap/roadmap.js
+++ app/roadmap/roadmap.js
@@ -13,14 +13,10 @@ angular.module('myApp.roadmap', ['ngRoute'])
     var url = 'https://learning-ninja-api.herokuapp.com/topics/6'
 
     $scope.httpGet = function (url) {
-
       httpFactory.httpGet(url).then(function (response) {
         var responseArray = response.data
         $scope.topics = responseArray
-
-    })
-
-
+      })
     }
 
     $scope.httpGet(url)

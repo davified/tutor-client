7391a6c1e30ea92a51e253fdae7b3700cfe92659
David Tan <davified@gmail.com>
2016-08-23 21:37:12 +0800

    completed dashboard

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -30,4 +30,19 @@ angular.module('myApp.dashboard', ['ngRoute', 'chart.js'])
         ]
       }
     }
-  }])
+// whole numbers
+    $scope.labels2 = ["Numbers up to 10 million", "Four operations", "Order of operations"];
+    $scope.data2 = [300, 500, 100];
+
+// fractions
+    $scope.labels3 = ["four operations on fractions", "dividing fractions"];
+    $scope.data3 = [500 , 200];
+
+// geometry
+    $scope.labels4 = ["Geometrical figures", "Nets", "Measurements"];
+    $scope.data4 = [300, 800, 20];
+
+// measurement
+    $scope.labels5 = ["Length, mass and volume", "Area of triangle", "Parallelogram, rhombus and trapezium", "Volume of cube and cuboid"];
+    $scope.data5 = [800, 50, 300, 200];
+}])
